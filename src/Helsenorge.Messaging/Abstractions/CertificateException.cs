a30abf6866f7015d2f323958e1d7d10620d62cca
Arne Bitubekk <arne.hb@gmail.com>
2018-03-12 09:36:43 +0100

    Include encrypted payload in exception if signature fails to validate
    * If we get an error when validating the signature of the signed
      payload, cast a CertificateException and include the original 
      encrypted payload.
    * PR #33

---
+++ src/Helsenorge.Messaging/Abstractions/CertificateException.cs
@@ -0,0 +1,25 @@
+﻿using System.Security;
+
+namespace Helsenorge.Messaging.Abstractions
+{
+    /// <summary>
+    ///     Exception related to signing certificate with inheritance to SecurityException
+    /// </summary>
+    public class CertificateException : SecurityException
+    {
+        /// <summary>
+        ///     Initiate a new instance
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="payload">Payload of message</param>
+        public CertificateException(string message, byte[] payload) : base(message)
+        {
+            Payload = payload;
+        }
+
+        /// <summary>
+        ///     Payload of message with error signing certificate
+        /// </summary>
+        public byte[] Payload { get; }
+    }
+}
