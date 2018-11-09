b57929d63f7718f58210dbfabb3cc0f8a69ca359
danielehelse <daniel.marston@ehelse.no>
2018-11-09 13:47:02 +0100

    Add exception for receiving application when message not supported (#50)
    * Currently if an unsupported message is received then an unexpected exception results and no information is sent to the client. Adding an unsupported message exception allows the message to be returned to the senders error queue with an transport:unsupported-message error.
    * Add exception for receiving application when message not supported

---
+++ src/Helsenorge.Messaging/ServiceBus/Receivers/UnsupportedMessageException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Helsenorge.Messaging.ServiceBus.Receivers
+{
+    [Serializable]
+    public class UnsupportedMessageException : Exception
+    {
+        public UnsupportedMessageException()
+        {
+        }
+
+        public UnsupportedMessageException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedMessageException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnsupportedMessageException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
