a0b9d3ea2aee1a53ba8590749f82f92ce2b1070e
Kenneth Myhra <kennethmyhra@gmail.com>
2019-12-05 13:54:18 +0100

    fix: Make test cases run on CI platform

--- test/Helsenorge.Registries.Tests/DistributedCacheFactory.cs
+++ test/Helsenorge.Registries.Tests/DistributedCacheFactory.cs
@@ -6,24 +6,9 @@ namespace Helsenorge.Registries.Tests
     {
         public static IDistributedCache Create()
         {
-            
-#if NET46
-            return CreateForNet46();
-#elif NET471
             return CreateForNet471();
-#else
-            throw new System.NotImplementedException("Unsupported target framework");
-#endif
-
         }
 
-#if NET46
-        private static IDistributedCache CreateForNet46()
-        {
-            var memoryCache = new Microsoft.Extensions.Caching.Memory.MemoryCache(new Microsoft.Extensions.Caching.Memory.MemoryCacheOptions());
-            return new MemoryDistributedCache(memoryCache);
-        }
-#elif NET471
         private static IDistributedCache CreateForNet471()
         {
             return new MemoryDistributedCache(new Net471MemoryDistributedCacheOptions());
@@ -33,6 +18,5 @@ namespace Helsenorge.Registries.Tests
         {
             public Microsoft.Extensions.Caching.Memory.MemoryDistributedCacheOptions Value => new Microsoft.Extensions.Caching.Memory.MemoryDistributedCacheOptions();
         }
-#endif
     }
 }
