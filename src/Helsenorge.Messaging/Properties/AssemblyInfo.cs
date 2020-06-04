84bc8705388d527e4df88509d9e08f4cfc1e4b51
Kenneth Myhra <kennethmyhra@gmail.com>
2020-06-04 21:53:19 +0200

    fix: Assemblies not signed
    * Assemblies where not signed if ran outside Visual Studio IDE
    * Removed SignAssembly attribute from .csproj files

---
+++ src/Helsenorge.Messaging/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyKeyFile("../../tools/key.snk")]
+
+[assembly: InternalsVisibleTo("Helsenorge.Messaging.Tests, PublicKey=00240000048" + 
+                              "0000094000000060200000024000052534131000400000100" +
+                              "0100773d7b513076df908a05edadb53a26effc169310d844f" +
+                              "f47ed842bebef258bcfda39e2c62e4d6ceabb04037a24bcd2" +
+                              "85efd882ee7623170664411d5f5107cd2756221ba572b4903" +
+                              "bd45153c192cae7c544ea23b88f814c49a4709218355eac93" +
+                              "98524c353f4a5678ea9a2755bb012707d2de25019af5fd511" +
+                              "b1f48aa5a6eadd4")]
+[assembly: InternalsVisibleTo("Helsenorge.Messaging.IntegrationTests, PublicKey=00240000048" +
+                              "0000094000000060200000024000052534131000400000100" +
+                              "0100773d7b513076df908a05edadb53a26effc169310d844f" +
+                              "f47ed842bebef258bcfda39e2c62e4d6ceabb04037a24bcd2" +
+                              "85efd882ee7623170664411d5f5107cd2756221ba572b4903" +
+                              "bd45153c192cae7c544ea23b88f814c49a4709218355eac93" +
+                              "98524c353f4a5678ea9a2755bb012707d2de25019af5fd511" +
+                              "b1f48aa5a6eadd4")]
