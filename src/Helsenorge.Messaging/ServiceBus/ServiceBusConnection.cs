1b0605abd33c009504a3ac4bbbb773db8e3b3e3d
Kenneth Myhra <kennethmyhra@gmail.com>
2020-12-17 09:35:00 +0100

    feat: MaxSessionsPerConnection and MaxLinksPerSession (#188)
    * Expose the properties MaxSessionPerConnection and MaxLinksPerSession
      from AMQPNetLite's:
      - ConnectionFactory.AMQP.MaxSessionPerConnection
      - ConnectionFactory.AMQP.MaxLinksPerSession

--- src/Helsenorge.Messaging/ServiceBus/ServiceBusConnection.cs
+++ src/Helsenorge.Messaging/ServiceBus/ServiceBusConnection.cs
@@ -15,7 +15,9 @@ namespace Helsenorge.Messaging.ServiceBus
 {
     internal class ServiceBusConnection
     {
+        private ConnectionFactory _connectionFactory;
         private readonly Address _address;
+        
 
         public ServiceBusHttpClient HttpClient { get; }
 
@@ -33,6 +35,12 @@ namespace Helsenorge.Messaging.ServiceBus
         }
 
         public ServiceBusConnection(string connectionString, ILogger logger)
+            : this(connectionString, ServiceBusSettings.DefaultMaxLinksPerSession, ServiceBusSettings.DefaultMaxSessions, logger)
+        {
+
+        }
+
+        public ServiceBusConnection(string connectionString, int maxLinksPerSession, ushort maxSessionsPerConnection, ILogger logger)
         {
             if (string.IsNullOrEmpty(connectionString))
             {
@@ -52,6 +60,16 @@ namespace Helsenorge.Messaging.ServiceBus
                 Namespace = _address.Path.Substring(1);
                 _address = new Address(connectionString.Substring(0, connectionString.Length - Namespace.Length));
             }
+
+            _connectionFactory = new ConnectionFactory();
+            if (maxLinksPerSession != ServiceBusSettings.DefaultMaxLinksPerSession)
+            {
+                _connectionFactory.AMQP.MaxLinksPerSession = maxLinksPerSession;
+            }
+            if (maxSessionsPerConnection != ServiceBusSettings.DefaultMaxSessions)
+            {
+                _connectionFactory.AMQP.MaxSessionsPerConnection = maxSessionsPerConnection;
+            }
         }
 
         /// <summary>
@@ -66,7 +84,7 @@ namespace Helsenorge.Messaging.ServiceBus
             }
             if (_connection == null || _connection.IsClosed)
             {
-                _connection = new Connection(_address);
+                _connection = _connectionFactory.CreateAsync(_address).GetAwaiter().GetResult();
                 return true;
             }
             return false;
