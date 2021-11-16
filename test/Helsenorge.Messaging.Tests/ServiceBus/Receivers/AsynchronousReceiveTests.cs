e3c3e088a6a8feb7ca0bed010ef4dcf3eaea33eb
Kenneth Myhra <kennethmyhra@gmail.com>
2021-11-16 21:43:22 +0100

    MessagingServer: Make MessagingServer mockable and add Mock
    We need to be able to mock out methods on MessagingServer. Therefore we
    have removed the sealed keyword from MessagingServer. Also added
    MockMessagingServer so that our tests pass.

--- test/Helsenorge.Messaging.Tests/ServiceBus/Receivers/AsynchronousReceiveTests.cs
+++ test/Helsenorge.Messaging.Tests/ServiceBus/Receivers/AsynchronousReceiveTests.cs
@@ -81,7 +81,7 @@ namespace Helsenorge.Messaging.Tests.ServiceBus.Receivers
             Client.ServiceBus.RegisterAlternateMessagingFactory(MockFactory);
 
             var partyBProtection = new SignThenEncryptMessageProtection(TestCertificates.HelsenorgePrivateSigntature, TestCertificates.HelsenorgePrivateEncryption);
-            Server = new MessagingServer(Settings, LoggerFactory, CollaborationRegistry, AddressRegistry, CertificateStore, CertificateValidator, partyBProtection);
+            Server = new MockMessagingServer(Settings, LoggerFactory, CollaborationRegistry, AddressRegistry, CertificateStore, CertificateValidator, partyBProtection);
             Server.ServiceBus.RegisterAlternateMessagingFactory(MockFactory);
 
             CollaborationRegistry.SetupFindAgreementForCounterparty(i =>
