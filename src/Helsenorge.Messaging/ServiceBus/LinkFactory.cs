e9846c6bc6a9b6665864d2e635a0724a6f55190f
Kenneth Myhra <kennethmyhra@gmail.com>
2021-11-22 15:03:46 +0100

    ServiceBus: Add LinkFactory and publicly expose ServiceBusConnection
    This exposes the class ServiceBusConnection publicly and adds a
    LinkFactory.
    
    The LinkFactory exposes a set of methods to which can be used to create
    Senders and Receivers. It also exposes a method which helps to create an
    outgoing message.

---
+++ src/Helsenorge.Messaging/ServiceBus/LinkFactory.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Copyright (c) 2021, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System.IO;
+using System.Threading.Tasks;
+using Amqp;
+using Amqp.Framing;
+using Helsenorge.Messaging.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Helsenorge.Messaging.ServiceBus
+{
+    public class LinkFactory
+    {
+        private readonly ServiceBusConnection _connection;
+        private readonly ILogger<LinkFactory> _logger;
+
+        public LinkFactory(ServiceBusConnection connection, ILogger<LinkFactory> logger)
+        {
+            _connection = connection;
+            _logger = logger;
+        }
+
+        public IMessagingReceiver CreateReceiver(string queue, int linkCredit = 25)
+            => new ServiceBusReceiver(_connection, queue, linkCredit, _logger);
+
+        public IMessagingSender CreateSender(string queue)
+            => new ServiceBusSender(_connection, queue, _logger);
+
+        public async Task<IMessagingMessage> CreateMessageAsync(int fromHerId, OutgoingMessage message, Stream payload)
+        {
+            using var payloadMemoryStream = new MemoryStream();
+            await payload.CopyToAsync(payloadMemoryStream);
+
+            var innerMessage = new Message
+            {
+                BodySection = new Data
+                {
+                    Binary = payloadMemoryStream.ToArray(),
+                }
+            };
+
+            return new ServiceBusMessage(innerMessage)
+            {
+                MessageId = message.MessageId,
+                MessageFunction = string.IsNullOrWhiteSpace(message.ReceiptForMessageFunction)
+                    ? message.MessageFunction
+                    : message.ReceiptForMessageFunction,
+                ToHerId = message.ToHerId,
+                FromHerId = fromHerId,
+                ScheduledEnqueueTimeUtc = message.ScheduledSendTimeUtc,
+
+            };
+        }
+    }
+}
