076b9ffb5bdae22b91f1afdba4290e4d6f1c4df6
Kenneth Myhra <kennethmyhra@gmail.com>
2021-12-02 22:16:39 +0100

    Examples: A simple example using a pooled sender
    This example shows how to use the new LinkFactoryPool for sending
    messages.

---
+++ Examples/PooledSender/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Copyright (c) 2021, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Helsenorge.Messaging;
+using Helsenorge.Messaging.Abstractions;
+using Helsenorge.Messaging.ServiceBus;
+using Microsoft.Extensions.Logging;
+
+namespace PooledSender
+{
+    class Program
+    {
+        private static readonly string _connectionString = "amqp://guest:guest@127.0.0.1:5672";
+        private static readonly string _queue = "/amq/queue/test-queue";
+
+        static async Task Main(string[] args)
+        {
+            var loggerFactory = new LoggerFactory();
+            var settings = new ServiceBusSettings
+            {
+                ConnectionString = _connectionString,
+            };
+
+            await using var linkFactoryPool = new LinkFactoryPool(settings, loggerFactory.CreateLogger<LinkFactoryPool>());
+            try
+            {
+                var messageCount = 20000;
+                var sender = await linkFactoryPool.CreateCachedMessageSender(_queue);
+                for (int i = 0; i < messageCount; i++)
+                {
+                    var outgoingMessage = new OutgoingMessage
+                    {
+                        MessageId = Guid.NewGuid().ToString("N")
+                    };
+                    var bodyString = $"Hello world! - {i + 1}";
+                    var body = new MemoryStream(Encoding.UTF8.GetBytes(bodyString));
+
+                    var message = await linkFactoryPool.CreateMessageAsync(123, outgoingMessage, body);
+
+                    await sender.SendAsync(message);
+
+                    Console.WriteLine($"Message Id: '{message.MessageId}'\nMessage Body: '{bodyString}'\nMessages sent: '{i + 1}'.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"An error occurred: '{e.Message}'.\nStack Trace: {e.StackTrace}");
+            }
+            finally
+            {
+                linkFactoryPool.ReleaseCachedMessageReceiver(_queue);
+            }
+        }
+    }
+}
