bd5137a5adce57b2542750b030a8244f626a700b
Kenneth Myhra <kennethmyhra@gmail.com>
2022-06-15 21:31:44 +0200

    Registries: Add convenience class for the ServiceBusManagerV2 contract
    This adds a convenience class to simplify working with the
    ServiceBusManagerV2 service contract in .NET Framework and .NET Core
    environments.

---
+++ src/Helsenorge.Registries/Abstractions/IServiceBusManager.cs
@@ -0,0 +1,49 @@
+/* 
+ * Copyright (c) 2022, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Helsenorge.Registries.Abstractions
+{
+
+    /*
+ 
+     */
+    /// <summary>
+    /// The IServiceBusManager interface.
+    /// </summary>
+    public interface IServiceBusManager
+    {
+        /// <summary>
+        /// Returns the subscriptions associated with the authenticated user.
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<EventSubscription>> GetSubscriptionsAsync();
+        
+        /// <summary>
+        /// Creates a subscription for the authenticated user.
+        /// </summary>
+        /// <param name="eventSource">Name of the event source</param>
+        /// <param name="systemIdentificator">A string representing the system identificator/name for the system that will use the subscription.Valid characters are uppercase, lowercase and underscore.</param>
+        /// <param name="eventName">Name of the event type. Can be omitted or an empty string if all event types for a given source are desired.
+        /// AddressRegister │ SubscriptionEventName.ArBusEvents.*
+        /// Hpr             │ SubscriptionEventName.HprBusEvents.*
+        /// Lsr             │ SubscriptionEventName.LsrBusEvents.*
+        /// Resh            │ SubscriptionEventName.ReshBusEvents.*
+        /// </param>
+        /// <returns>returns an instance of a <see cref="EventSubscription"/> containing information about the Subscription.</returns>
+        Task<EventSubscription> SubscribeAsync(SubscriptionEventSource eventSource, string systemIdentificator, string eventName = null);
+
+        /// <summary>
+        /// Deletes and ends the subscriptions.
+        /// </summary>
+        /// <param name="queueName">The name of the queue to delete. This information was obtained from the class <see cref="EventSubscription"/>.</param>
+        Task UnsubscribeAsync(string queueName);
+    }
+}
---
+++ src/Helsenorge.Registries/ServiceBusManager.cs
@@ -0,0 +1,79 @@
+/* 
+ * Copyright (c) 2022, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Helsenorge.Registries.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Helsenorge.Registries.Utilities
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ServiceBusManager : IServiceBusManager
+    {
+        private readonly SoapServiceInvoker _invoker;
+        private readonly ILogger<ServiceBusManager> _logger;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="settings"></param>
+        /// <param name="logger"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public ServiceBusManager(ServiceBusManagerSettings settings, ILogger<ServiceBusManager> logger)
+        {
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+            
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _invoker = new SoapServiceInvoker(settings.WcfConfiguration);
+        }
+
+        /// <inheritdoc cref="IServiceBusManager.GetSubscriptionsAsync"/>
+        public async Task<IEnumerable<EventSubscription>> GetSubscriptionsAsync()
+        {
+            var subscriptions = await GetSubscriptionsAsyncInternal();
+            return subscriptions.AsEnumerable();
+        }
+
+        /// <inheritdoc cref="IServiceBusManager.SubscribeAsync"/>
+        public Task<EventSubscription> SubscribeAsync(SubscriptionEventSource eventSource, string systemIdentificator, string eventName = null)
+            => SubscribeAsyncInternal(eventSource, eventName ?? string.Empty, systemIdentificator);
+
+        /// <inheritdoc cref="IServiceBusManager.UnsubscribeAsync"/>
+        public Task UnsubscribeAsync(string queueName)
+            => UnsubscribeAsyncInternal(queueName);
+
+        /// <inheritdoc cref="IServiceBusManager.GetSubscriptionsAsync"/>
+        [ExcludeFromCodeCoverage]
+        protected async virtual Task<EventSubscription[]> GetSubscriptionsAsyncInternal()
+            => await Invoke(sbm => sbm.GetSubscriptionsAsync(), "GetSubscriptionsAsync").ConfigureAwait(false);
+        
+        /// <inheritdoc cref="IServiceBusManager.SubscribeAsync"/>
+        [ExcludeFromCodeCoverage]
+        protected async virtual Task<EventSubscription> SubscribeAsyncInternal(SubscriptionEventSource eventSource, string systemIdentificator, string eventName)
+            => await Invoke(sbm => sbm.SubscribeAsync(eventSource, eventName, systemIdentificator), "SubscribeAsync").ConfigureAwait(false);
+
+        /// <inheritdoc cref="IServiceBusManager.UnsubscribeAsync"/>
+        [ExcludeFromCodeCoverage]
+        protected async virtual Task UnsubscribeAsyncInternal(string queueName)
+            => await Invoke(sbm => sbm.UnsubscribeAsync(queueName), "UnsubscribeAsync").ConfigureAwait(false);
+
+        [ExcludeFromCodeCoverage] // Requires wire communication.
+        private Task<T> Invoke<T>(Func<IServiceBusManagerV2, Task<T>> action, string methodName)
+            => _invoker.Execute(_logger, action, methodName);
+        
+        [ExcludeFromCodeCoverage] // Requires wire communication.
+        private Task Invoke(Func<IServiceBusManagerV2, Task> action, string methodName)
+            => _invoker.Execute(_logger, action, methodName);
+    }
+}
