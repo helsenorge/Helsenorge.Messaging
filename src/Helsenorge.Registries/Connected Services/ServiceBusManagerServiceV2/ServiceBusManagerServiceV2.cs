68e8af55b9c7ae90b9e745e968ef0004693a0252
Kenneth Myhra <kennethmyhra@gmail.com>
2022-08-31 20:02:21 +0200

    Wcf: Add new keyword to hide inherited member CloseAsync()

--- src/Helsenorge.Registries/Connected Services/ServiceBusManagerServiceV2/ServiceBusManagerServiceV2.cs
+++ src/Helsenorge.Registries/Connected Services/ServiceBusManagerServiceV2/ServiceBusManagerServiceV2.cs
@@ -224,7 +224,7 @@ namespace Helsenorge.Registries
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
         }
         
-        public virtual System.Threading.Tasks.Task CloseAsync()
+        public virtual new System.Threading.Tasks.Task CloseAsync()
         {
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
         }
