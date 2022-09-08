d3e571b0ba8607b6053dbad6129b6a9dd7634228
Kenneth Myhra <kennethmyhra@gmail.com>
2022-09-08 14:58:10 +0200

    Wcf: Disable warning CS0108 for CloseAsync

--- src/Helsenorge.Registries/Connected Services/ServiceBusManagerServiceV2/ServiceBusManagerServiceV2.cs
+++ src/Helsenorge.Registries/Connected Services/ServiceBusManagerServiceV2/ServiceBusManagerServiceV2.cs
@@ -224,7 +224,9 @@ namespace Helsenorge.Registries
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
         }
         
-        public virtual new System.Threading.Tasks.Task CloseAsync()
+#pragma warning disable CS0108
+        public virtual System.Threading.Tasks.Task CloseAsync()
+#pragma warning restore CS0108
         {
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
         }
