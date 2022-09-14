helsenorge/Helsenorge.Messaging

commit b0243b9f576392d863952f4406989584bb183253

ServiceBus: Log hostname and queue name when starting send operation

src/Helsenorge.Messaging/ServiceBus/ServiceBusCore.cs
@@ -125,7 +125,13 @@ namespace Helsenorge.Messaging.ServiceBus
             if (string.IsNullOrEmpty(outgoingMessage.MessageId)) throw new ArgumentNullException(nameof(outgoingMessage.MessageId));
             if (outgoingMessage.Payload == null) throw new ArgumentNullException(nameof(outgoingMessage.Payload));
 
-            logger.LogStartSend(queueType, outgoingMessage.MessageFunction, Core.Settings.MyHerId, outgoingMessage.ToHerId, outgoingMessage.MessageId, outgoingMessage.Payload);
+            // when we are replying to a synchronous message, we need to use the replyto of the original message
+            var queueName =
+                (queueType == QueueType.SynchronousReply) ?
+                    replyTo :
+                    await ConstructQueueName(logger, outgoingMessage.ToHerId, queueType).ConfigureAwait(false);
+
+            logger.LogStartSend(queueType, outgoingMessage.MessageFunction, Core.Settings.MyHerId, outgoingMessage.ToHerId, outgoingMessage.MessageId, $"Sending message using host and queue: '{HostnameAndPath}/{queueName}'", outgoingMessage.Payload);
 
             var hasAgreement = true;
             // first we try and find an agreement
@@ -211,17 +217,10 @@ namespace Helsenorge.Messaging.ServiceBus
             logger.LogAfterFactoryPoolCreateMessage(outgoingMessage.MessageFunction, Core.Settings.MyHerId, outgoingMessage.ToHerId, outgoingMessage.MessageId);
 
             if (queueType != QueueType.SynchronousReply)
-            {
-                messagingMessage.ReplyTo = 
-                    replyTo ?? await ConstructQueueName(logger, Core.Settings.MyHerId, queueType).ConfigureAwait(false);
-            }
+                messagingMessage.ReplyTo = queueName;
             messagingMessage.ContentType = Core.MessageProtection.ContentType;
             messagingMessage.MessageId = outgoingMessage.MessageId;
-            // when we are replying to a synchronous message, we need to use the replyto of the original message
-            messagingMessage.To = 
-                (queueType == QueueType.SynchronousReply) ? 
-                replyTo : 
-                await ConstructQueueName(logger, outgoingMessage.ToHerId, queueType).ConfigureAwait(false);
+            messagingMessage.To = queueName;
             messagingMessage.MessageFunction = outgoingMessage.MessageFunction;
             messagingMessage.CorrelationId = correlationId ?? outgoingMessage.MessageId;
             messagingMessage.TimeToLive = (queueType == QueueType.Asynchronous)

src/Helsenorge.Messaging/ServiceBus/ServiceBusLoggingExtensions.cs
@@ -17,7 +17,7 @@ namespace Helsenorge.Messaging.ServiceBus
     {
         private static readonly Action<ILogger, QueueType, string, int, int, string, Exception> StartReceive;
         private static readonly Action<ILogger, QueueType, string, int, int, string, Exception> EndReceive;
-        private static readonly Action<ILogger, QueueType, string, int, int, string, Exception> StartSend;
+        private static readonly Action<ILogger, QueueType, string, int, int, string, string, Exception> StartSend;
         private static readonly Action<ILogger, QueueType, string, int, int, string, Exception> EndSend;
         private static readonly Action<ILogger, string, int, int, string, string, Exception> ResponseTime;
         private static readonly Action<ILogger, string, string, int, Exception> LogTimeout;
@@ -52,9 +52,9 @@ namespace Helsenorge.Messaging.ServiceBus
             EndReceive(logger, queueType, message.MessageFunction, message.FromHerId, message.ToHerId, message.MessageId, null);
         }
 
-        public static void LogStartSend(this ILogger logger, QueueType queueType, string function, int fromHerId, int toHerId, string messageId, XDocument xml)
+        public static void LogStartSend(this ILogger logger, QueueType queueType, string function, int fromHerId, int toHerId, string messageId, string additionalData, XDocument xml)
         {
-            StartSend(logger, queueType, function, fromHerId, toHerId, messageId,  null);
+            StartSend(logger, queueType, function, fromHerId, toHerId, messageId, additionalData,  null);
             if (xml != null)
             {
                 logger.LogDebug(xml.ToString());
@@ -151,10 +151,10 @@ namespace Helsenorge.Messaging.ServiceBus
                 EventIds.ServiceBusReceive,
                 "End-ServiceBusReceive{QueueType}: {MessageFunction} FromHerId: {FromHerId} ToHerId: {ToHerId} MessageId: {MessageId}");
 
-            StartSend = LoggerMessage.Define<QueueType, string, int, int, string>(
+            StartSend = LoggerMessage.Define<QueueType, string, int, int, string, string>(
                 LogLevel.Information,
                 EventIds.ServiceBusSend,
-                "Start-ServiceBusSend{QueueType}: {MessageFunction} FromHerId: {FromHerId} ToHerId: {ToHerId} MessageId: {MessageId}");
+                "Start-ServiceBusSend{QueueType}: {MessageFunction} FromHerId: {FromHerId} ToHerId: {ToHerId} MessageId: {MessageId} Additional Data: {AdditionalData}");
 
             EndSend = LoggerMessage.Define<QueueType, string, int, int, string>(
                 LogLevel.Information,

commit 9349117b7adb3f4edabe48765312018cad6e2a50

ServiceBus: Properly set ReplyTo
This bug was introduced in commit: b0243b9.

src/Helsenorge.Messaging/ServiceBus/ServiceBusCore.cs
@@ -217,7 +217,7 @@ namespace Helsenorge.Messaging.ServiceBus
             logger.LogAfterFactoryPoolCreateMessage(outgoingMessage.MessageFunction, Core.Settings.MyHerId, outgoingMessage.ToHerId, outgoingMessage.MessageId);
 
             if (queueType != QueueType.SynchronousReply)
-                messagingMessage.ReplyTo = queueName;
+                messagingMessage.ReplyTo = replyTo ?? queueName;
             messagingMessage.ContentType = Core.MessageProtection.ContentType;
             messagingMessage.MessageId = outgoingMessage.MessageId;
             messagingMessage.To = queueName;