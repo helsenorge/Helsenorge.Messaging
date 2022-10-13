43a28cdce25f4689a84675e07e58a26019546e91
Kenneth Myhra <kennethmyhra@gmail.com>
2022-10-13 11:56:30 +0200

    Registries: Add DeliveryChannel to DummyCollaborationProtocolProfile
    This adds an asynchronous DeliveryChannel to the Dummy instance of our
    CollaborationProtocolProfile.
    
    This should solve the issue for AppRecs when the other party do not have
    a CPP configured.
    
    This is hack to continue our support for interns and substitues lacking
    CPP.

--- src/Helsenorge.Registries/CollaborationProtocolRegistry.cs
+++ src/Helsenorge.Registries/CollaborationProtocolRegistry.cs
@@ -114,9 +114,14 @@ namespace Helsenorge.Registries
             }
             if (string.IsNullOrEmpty(xmlString))
             {
+                // HACK: This whole thing is a hack and should be removed.
+                var communicationParty = await _adressRegistry.FindCommunicationPartyDetailsAsync(logger, counterpartyHerId).ConfigureAwait(false);
+                // HACK: We are assuming that we should return the asynchronous queue name as the delivery channel.
+                var deliveryChannel = communicationParty.AsynchronousQueueName;
                 result = CreateDummyCollaborationProtocolProfile(counterpartyHerId,
                     await _adressRegistry.GetCertificateDetailsForEncryptionAsync(logger, counterpartyHerId).ConfigureAwait(false),
-                    await _adressRegistry.GetCertificateDetailsForValidatingSignatureAsync(logger, counterpartyHerId).ConfigureAwait(false));
+                    await _adressRegistry.GetCertificateDetailsForValidatingSignatureAsync(logger, counterpartyHerId).ConfigureAwait(false),
+                    deliveryChannel);
             }
             else
             {
@@ -342,11 +347,35 @@ namespace Helsenorge.Registries
         protected virtual Task<string> GetCollaborationProtocolProfileAsXmlAsyncInternal(ILogger logger, Guid id)
             => Invoke(logger, x => x.GetCppXmlAsync(id), "GetCppXmlAsync");
         
-        private CollaborationProtocolProfile CreateDummyCollaborationProtocolProfile(int herId, Abstractions.CertificateDetails encryptionCertificate, Abstractions.CertificateDetails signatureCertificate)
+        // HACK: This is a hack used for when interns and substitues which do not have CPP send a message on behalf of the GP.
+        private CollaborationProtocolProfile CreateDummyCollaborationProtocolProfile(int herId, Abstractions.CertificateDetails encryptionCertificate, Abstractions.CertificateDetails signatureCertificate, string deliveryChannel)
         {
             return new CollaborationProtocolProfile
             {
-                Roles = new List<CollaborationProtocolRole>(),
+                Roles = new List<CollaborationProtocolRole>
+                {
+                    new CollaborationProtocolRole
+                    {
+                        ReceiveMessages = new List<CollaborationProtocolMessage>
+                        {
+                            new CollaborationProtocolMessage
+                            {
+                                Name = "APPREC",
+                                DeliveryProtocol = DeliveryProtocol.Amqp,
+                                DeliveryChannel = deliveryChannel
+                            }
+                        },
+                        SendMessages = new List<CollaborationProtocolMessage>
+                        {
+                            new CollaborationProtocolMessage
+                            {
+                                Name = "APPREC",
+                                DeliveryProtocol = DeliveryProtocol.Amqp,
+                                DeliveryChannel = deliveryChannel
+                            }
+                        }
+                    }
+                },
                 HerId = herId,
                 Name = DummyPartyName,
                 EncryptionCertificate = encryptionCertificate?.Certificate,
