95849e96ba01e8736cb1624e5d0a0ab2e7fe9634
Kenneth Myhra <kennethmyhra@gmail.com>
2022-10-26 17:16:24 +0200

    AdminLib: Add ConstructQueueName()
    This patch adds ConstructQueueName() which constructs a queue name from
    a HER-id and QueueType according to our naming rules.

---
+++ src/Helsenorge.Messaging.AdminLib/QueueUtilities.cs
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System;
+using Helsenorge.Messaging.ServiceBus;
+
+namespace Helsenorge.Messaging.AdminLib;
+
+internal static class QueueUtilities
+{
+    internal static string ConstructQueueName(int herId, QueueType queueType)
+    {
+        return queueType switch
+        {
+            QueueType.Asynchronous => $"{herId}_async",
+            QueueType.Synchronous => $"{herId}_sync",
+            QueueType.Error => $"{herId}_error",
+            QueueType.DeadLetter => $"{herId}_dl",
+            QueueType.SynchronousReply => $"{herId}_syncreply",
+            _ => throw new ArgumentOutOfRangeException(nameof(queueType), queueType, $"The queue type: '{queueType}' is not supported."),
+        };
+    }
+}
