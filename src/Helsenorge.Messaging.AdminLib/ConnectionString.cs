2f1cee3b71962932d157f7b69d075eb2702ae972
Kenneth Myhra <kennethmyhra@gmail.com>
2022-10-26 13:40:40 +0200

    AdminLib: Add ConnectionString abstraction

---
+++ src/Helsenorge.Messaging.AdminLib/ConnectionString.cs
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System.Collections.Generic;
+
+namespace Helsenorge.Messaging.AdminLib;
+
+public class ConnectionString
+{
+    public string HostName { get; set; }
+    public int Port { get; set; }
+    public string VirtualHost { get; set; } = "/";
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Exchange { get; set; }
+    public string ClientProvidedName { get; set; }
+}
