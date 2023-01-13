9644ffdd57dda378ad27043346eb27420c69f455
Kenneth Myhra <kennethmyhra@gmail.com>
2023-01-13 11:26:53 +0100

    Registries: Add extension method ToInt() for string types

---
+++ src/Helsenorge.Registries/Utilities/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Copyright (c) 2023, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System.Globalization;
+
+namespace Helsenorge.Registries.Utilities;
+
+public static class StringExtensions
+{
+    public static int ToInt(this string value, int defaultValue = 0)
+    {
+        return int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var i)
+            ? i
+            : defaultValue;
+    }
+}
