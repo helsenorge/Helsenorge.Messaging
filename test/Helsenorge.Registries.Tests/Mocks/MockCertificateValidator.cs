c160586b32064bcffd8dd36f73c91f895ae0517f
Arne Hjelle Bitubekk <Arne.Hjelle.Bitubekk@nhn.no>
2023-01-25 14:50:36 +0100

    Registries: Add tests for support old CPA/CPP Cache
    Added tests that tests the fallback where Name is used if Action is
    null. Action is null if the old object is cached which does not contain
    Action.

---
+++ test/Helsenorge.Registries.Tests/Mocks/MockCertificateValidator.cs
@@ -0,0 +1,28 @@
+﻿/* 
+ * Copyright (c) 2020, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+using Helsenorge.Registries.Abstractions;
+
+namespace Helsenorge.Registries.Tests.Mocks
+{
+    internal class MockCertificateValidator : ICertificateValidator
+    {
+        private Func<X509Certificate2, X509KeyUsageFlags, CertificateErrors> _error;
+
+        public void SetError(Func<X509Certificate2, X509KeyUsageFlags, CertificateErrors> func) => _error = func;
+
+        public CertificateErrors Validate(X509Certificate2 certificate, X509KeyUsageFlags usage)
+        {
+            if(certificate == null) return CertificateErrors.Missing;
+
+            return _error?.Invoke(certificate, usage) ?? CertificateErrors.None;
+        }
+    }
+}
