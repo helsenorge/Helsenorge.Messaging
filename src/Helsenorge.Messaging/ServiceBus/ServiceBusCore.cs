776e0facb877711092cfe3489e5209a10b9fd623
Stian Haugen Pettersen <stian_pettersen87@hotmail.com>
2023-02-03 11:20:52 +0100

    Added start and end logging on SendError

--- src/Helsenorge.Messaging/ServiceBus/ServiceBusCore.cs
+++ src/Helsenorge.Messaging/ServiceBus/ServiceBusCore.cs
@@ -294,7 +294,7 @@ namespace Helsenorge.Messaging.ServiceBus
                 logger.LogWarning(EventIds.MissingField, "FromHerId is missing. No idea where to send the error");
                 return;
             }
-            
+
             // Clones original message, but leaves out the payload
             var clonedMessage = originalMessage.Clone(false);
             // update some properties on the cloned message
@@ -339,9 +339,14 @@ namespace Helsenorge.Messaging.ServiceBus
                     clonedMessage.SetApplicationProperty(ErrorConditionDataHeaderKey, additionDataValue);
                 }
             }
-            logger.LogWarning("Reporting error to sender. FromHerId: {0} ToHerId: {1} ErrorCode: {2} ErrorDescription: {3} AdditionalData: {4}",  originalMessage.FromHerId, originalMessage.ToHerId, errorCode, errorDescription, additionDataValue);
+
+            logger.LogWarning("Reporting error to sender. FromHerId: {0} ToHerId: {1} ErrorCode: {2} ErrorDescription: {3} AdditionalData: {4}", originalMessage.FromHerId, originalMessage.ToHerId, errorCode, errorDescription, additionDataValue);
+
+            logger.LogStartSend(QueueType.Error, clonedMessage.MessageFunction, Core.Settings.MyHerId, clonedMessage.ToHerId, clonedMessage.MessageId, additionDataValue, null);
             await Send(logger, clonedMessage).ConfigureAwait(false);
+            logger.LogEndSend(QueueType.Error, clonedMessage.MessageFunction, Core.Settings.MyHerId, clonedMessage.ToHerId, clonedMessage.MessageId);
         }
+
         /// <summary>
         /// Gets the queue name that we can use on messages from a more extensive name
         /// </summary>
