3fcfdd52ea9143bf34991b5f03371c041a822bd5
Kenneth Myhra <kennethmyhra@gmail.com>
2023-03-15 15:44:04 +0100

    Messaging: Clean up in-line documentation for QueueNameNotSetException

--- src/Helsenorge.Messaging/Amqp/Exceptions/QueueNameNotSetException.cs
+++ src/Helsenorge.Messaging/Amqp/Exceptions/QueueNameNotSetException.cs
@@ -10,8 +10,15 @@ using System;
 
 namespace Helsenorge.Messaging.Amqp.Exceptions;
 
+/// <summary>
+/// Exception being thrown when queue name is empty / not set.
+/// </summary>
 public class QueueNameNotSetException : Exception
 {
+    /// <summary>
+    /// Constructs an instance of <see cref="QueueNameNotSetException"/>.
+    /// </summary>
+    /// <param name="message">The exception message.</param>
     public QueueNameNotSetException(string message)
         : base(message)
     {
