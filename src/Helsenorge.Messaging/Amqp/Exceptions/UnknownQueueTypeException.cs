fe76b58555b89094de3ce20710099b64728654f7
Kenneth Myhra <kennethmyhra@gmail.com>
2023-03-15 15:45:03 +0100

    Messaging: Clean up in-line documentation for UnknownQueueTypeException

--- src/Helsenorge.Messaging/Amqp/Exceptions/UnknownQueueTypeException.cs
+++ src/Helsenorge.Messaging/Amqp/Exceptions/UnknownQueueTypeException.cs
@@ -10,18 +10,32 @@ using System;
 
 namespace Helsenorge.Messaging.Amqp.Exceptions;
 
+/// <summary>
+/// Exception being thrown when the queue type is unknown.
+/// </summary>
 public class UnknownQueueTypeException : Exception
 {
+    /// <summary>
+    /// Constructs an instance of <see cref="UnknownQueueTypeException"/>.
+    /// </summary>
+    /// <param name="queueType">The queue type that is unknown.</param>
     public UnknownQueueTypeException(QueueType queueType)
         : base($"Unknown Queue Type: {queueType}")
     {
         QueueType = queueType;
     }
 
+    /// <summary>
+    /// Constructs an instance of <see cref="UnknownQueueTypeException"/>.
+    /// </summary>
+    /// <param name="message">The exception message.</param>
     public UnknownQueueTypeException(string message)
         : base(message)
     {
     }
 
+    /// <summary>
+    /// The queue type that was reported to be unknown.
+    /// </summary>
     public QueueType QueueType { get; }
 }
