ee06f2dfc5e7e39aaba7fb773000b7606439df87
Kenneth Myhra <kennethmyhra@gmail.com>
2023-03-15 15:28:57 +0100

    Registries: Clean up in-line documentation for StringExtensions

--- src/Helsenorge.Registries/Utilities/StringExtensions.cs
+++ src/Helsenorge.Registries/Utilities/StringExtensions.cs
@@ -10,8 +10,17 @@ using System.Globalization;
 
 namespace Helsenorge.Registries.Utilities;
 
+/// <summary>
+/// Contains extension methods for String.
+/// </summary>
 public static class StringExtensions
 {
+    /// <summary>
+    /// Returns an integer parsed from the string, otherwise the value set in defaultValue.
+    /// </summary>
+    /// <param name="value">The string to be converted to an integer.</param>
+    /// <param name="defaultValue">The default value to be returned if the integer value cannot be parsed.</param>
+    /// <returns></returns>
     public static int ToInt(this string value, int defaultValue = 0)
     {
         return int.TryParse(value, NumberStyles.Integer, CultureInfo.InvariantCulture, out var i)
