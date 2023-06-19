23da2f46f269f1c09a6db350ba69817a7762bbc4
Kenneth Myhra <kennethmyhra@gmail.com>
2023-06-19 15:45:10 +0200

    Messaging: Update XML Documentation on IMessagingNotification

--- src/Helsenorge.Messaging/Abstractions/IMessagingNotification.cs
+++ src/Helsenorge.Messaging/Abstractions/IMessagingNotification.cs
@@ -19,59 +19,71 @@ namespace Helsenorge.Messaging.Abstractions
     public interface IMessagingNotification
     {
         /// <summary>
-        /// Called to notify that an asynchronous message is ready for processing
+        /// Called when the asynchronous message is ready for processing.
         /// </summary>
-        /// <param name="message">Information about the message</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifyAsynchronousMessageReceivedAsync(IncomingMessage message);
+
         /// <summary>
-        /// Called to notify that we are starting to process an asynchronous message
+        /// Called to notify that processing of an asynchronous message has started. The client can do any necessary
+        /// set up it needs, like setting up CorrelationIds, etc.
         /// </summary>
-        /// <param name="listener">Reference to the listener which invoked the callback.</param>
-        /// <param name="message">Information about the message</param>
+        /// <param name="listener">Reference to the listener invoking the callback.</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifyAsynchronousMessageReceivedStartingAsync(MessageListener listener, IncomingMessage message);
+
         /// <summary>
-        /// Called to notify that we are finished processing an asynchronous message
+        /// Called when the asynchronous message has been successfully processed.
         /// </summary>
-        /// <param name="message">Information about the message</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifyAsynchronousMessageReceivedCompletedAsync(IncomingMessage message);
+
         /// <summary>
-        /// Called to notify that we are ready to process an error message
+        /// Called when the error message is ready for processing.
         /// </summary>
-        /// <param name="message"></param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifyErrorMessageReceivedAsync(IAmqpMessage message);
+
         /// <summary>
-        /// Called to notify that we are starting to process an error message
+        /// Called to notify that processing of an error message has started. The client can do any necessary set up it
+        /// needs, like setting up CorrelationIds, etc.
         /// </summary>
-        /// <param name="message">Information about the message</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifyErrorMessageReceivedStartingAsync(IncomingMessage message);
+
         /// <summary>
-        /// Called to notify that a synchronous message is ready for processing
+        /// Called when the synchronous message is ready for processing. The client should process the synchronous
+        /// message here and return the XDocument that is the output to the caller.
         /// </summary>
-        /// <param name="message">Information about the message</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
+        /// <returns>Returns the XDocument that is to be delivered to the synchronous caller.</returns>
         Task<XDocument> NotifySynchronousMessageReceivedAsync(IncomingMessage message);
+
         /// <summary>
-        /// Called to notify that we are finished to processing a synchronous message
+        /// Called when the synchronous message has been successfully processed.
         /// </summary>
-        /// <param name="message">Information about the message</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifySynchronousMessageReceivedCompletedAsync(IncomingMessage message);
+
         /// <summary>
-        /// Called to notify that we are starting to process a synchronous message
+        /// Called to notify that we haved started to process a synchronous message.
+        /// The client can do any necessary set up it needs, like setting up CorrelationIds, etc.
         /// </summary>
-        /// <param name="message">Information about the message</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
         Task NotifySynchronousMessageReceivedStartingAsync(IncomingMessage message);
 
         /// <summary>
-        /// Called to notifiy that we have an unhandled exception
+        /// Called to notifiy an unhandled exception has occurred. The client need to assert what it needs to do.
         /// </summary>
-        /// <param name="message">Information about the incoming message</param>
-        /// <param name="ex">The exception</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
+        /// <param name="ex">The exception that occurred.</param>
         Task NotifyUnhandledExceptionAsync(IAmqpMessage message, Exception ex);
 
         /// <summary>
-        /// Called to notifiy that we have a handled exception
+        /// Called to notify that an exception has occurred and that it has been handled.
         /// </summary>
-        /// <param name="message">Information about the incoming message</param>
-        /// <param name="ex">The exception</param>
+        /// <param name="message">The actual message, contains the payload in addition to metadata.</param>
+        /// <param name="ex">The exception that occurred.</param>
         Task NotifyHandledExceptionAsync(IAmqpMessage message, Exception ex);
     }
 }
