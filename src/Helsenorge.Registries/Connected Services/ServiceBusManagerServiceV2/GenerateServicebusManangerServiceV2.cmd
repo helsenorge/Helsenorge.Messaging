5f47586a1819ebfb79a91d239f9795998e8fd4d2
Kenneth Myhra <kennethmyhra@gmail.com>
2024-02-15 09:22:37 +0100

    Registries: Regenerate ServiceBusManagerServiceV2 WCF files
    This is done as a preparation task before adding a .NET 8 target. A few
    constructor signatures that were used in the previous generated files
    are no longer available in .NET 8.

--- src/Helsenorge.Registries/Connected Services/ServiceBusManagerServiceV2/GenerateServicebusManangerServiceV2.cmd
+++
@@ -1,7 +0,0 @@
-REM 1. Download latest WSDL from https://ws-web.test.nhn.no/v1/Business?singleWsdl
-REM 2. Open VS CLI: Visual Studio > Tools > Command Line > Developer Command Prompt
-REM 3. Navigate to folder: cd "src\Helsenorge.Registries\Connected Services\CPAService"
-REM 4. Execute	GenerateClientServicebusManangerServiceV2.cmd 
-
-svcutil "servicebusmanager_v2_singleWsdl.wsdl" /out:"ServiceBusManagerServiceV2.cs" /namespace:*,"Helsenorge.Registries"
-
