422484b9bd8475070025ace1b6b348567631cbb7
Arne Hjelle Bitubekk <arne.hjelle.bitubekk@nhn.no>
2024-05-13 10:40:39 +0200

    Added support for new CPPA Rest endpoint
    - HelseId integration to get authentication token
    -- Caching of authentication token
    - HttpClient to communicate with new CPPA Rest endpoint
    -- Added support for proxy
    - Added own implementation of ICollaborationProtocolRegistry called
      CollaborationProtocolRegistryRest which communicated with new rest
      service

---
+++ src/Helsenorge.Registries/Connected Services/HelseId/TokenCache.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Copyright (c) 2020-2024, Norsk Helsenett SF and contributors
+ * See the file CONTRIBUTORS for details.
+ *
+ * This file is licensed under the MIT license
+ * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
+ */
+
+using System;
+using System.Collections.Concurrent;
+
+namespace Helsenorge.Registries.Connected_Services.HelseId;
+
+public static class TokenCache
+{
+    private static readonly ConcurrentDictionary<string, (string Token, DateTime Expiry)> _cache = new();
+
+    public static bool TryGetToken(string key, out string token)
+    {
+        if (_cache.TryGetValue(key, out var cacheEntry) && DateTime.UtcNow < cacheEntry.Expiry)
+        {
+            token = cacheEntry.Token;
+            return true;
+        }
+
+        token = null;
+        return false;
+    }
+
+    public static void SetToken(string key, string token, int seconds)
+    {
+        var expiry = DateTime.UtcNow.AddSeconds(seconds);
+        _cache[key] = (token, expiry);
+    }
+}
