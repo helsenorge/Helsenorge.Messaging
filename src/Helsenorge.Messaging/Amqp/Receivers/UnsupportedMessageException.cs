73927ef2a27c658390d8fb55c44391db8e6cd999
Kenneth Myhra <kennethmyhra@gmail.com>
2025-01-28 10:42:24 +0100

    Messaging: Remove legacy serialization support ctor
    This ctor has been obsoleted from .net 8.0.

--- src/Helsenorge.Messaging/Amqp/Receivers/UnsupportedMessageException.cs
+++ src/Helsenorge.Messaging/Amqp/Receivers/UnsupportedMessageException.cs
@@ -43,15 +43,5 @@ namespace Helsenorge.Messaging.Amqp.Receivers
         public UnsupportedMessageException(string message, Exception innerException) : base(message, innerException)
         {
         }
-
-        /// <summary>Initializes a new instance of the <see cref="UnsupportedMessageException" /> class with serialized data.</summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo" /> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext" /> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">
-        /// <paramref name="info" /> is <see langword="null" />.</exception>
-        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is <see langword="null" /> or <see cref="P:System.Exception.HResult" /> is zero (0).</exception>
-        protected UnsupportedMessageException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
-        }
     }
 }
