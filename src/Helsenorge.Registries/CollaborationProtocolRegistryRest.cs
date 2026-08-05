202ccff5dd002bbd8ddc886cdee971263c453e46
Stian Haugen Pettersen <stian_pettersen87@hotmail.com>
2026-08-05 13:20:19 +0200

    Utivder Could not find or resolve protocol for counterparty med mer info (#853)

--- src/Helsenorge.Registries/CollaborationProtocolRegistryRest.cs
+++ src/Helsenorge.Registries/CollaborationProtocolRegistryRest.cs
@@ -6,6 +6,13 @@
  * available at https://raw.githubusercontent.com/helsenorge/Helsenorge.Messaging/master/LICENSE
  */
 
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using HelseId.Library.ClientCredentials.Interfaces;
 using HelseId.Library.Configuration;
 using HelseId.Library.Interfaces.JwtTokens;
@@ -15,13 +22,6 @@ using Helsenorge.Registries.Configuration;
 using Helsenorge.Registries.Utilities;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace Helsenorge.Registries;
 
@@ -111,9 +111,9 @@ public class CollaborationProtocolRegistryRest : ICollaborationProtocolRegistry
                     Data = { { "HerId", counterpartyHerId } }
                 };
             }
-
+          
             // if this happens, we fall back to the dummy profile further down
-            _logger.LogWarning(ex, "Could not find or resolve protocol for counterparty when using HerId {CounterpartyHerId}, fallback to generate dummy profile.", counterpartyHerId);
+            _logger.LogWarning(ex, "Could not find or resolve protocol for counterparty when using HerId {CounterpartyHerId}. StatusCode: {StatusCode}. Message: {Message}, fallback to generate dummy profile.", counterpartyHerId,ex.StatusCode, ex.Message);
         }
         if (string.IsNullOrEmpty(xmlString))
         {
